b0792ecda1a209c3f8b5dc05b81a0466897f6fa9
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2023-01-13 23:09:58 +0530

    stuck on firebase authentication

--- src/screens/SignInScreen/SignInScreen.jsx
+++ src/screens/SignInScreen/SignInScreen.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { auth } from '../../firebase';
 import './SignInScreen.css';
 
 const SignInScreen = () => {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
   const register = (e) => {
     e.preventDefault();
+
+    // When Register Button is Clicked Make a new Account with Email and Password
+    auth
+      .createUserWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
   };
 
   const signIn = (e) => {
@@ -14,8 +31,8 @@ const SignInScreen = () => {
     <div className="signInScreen">
       <form>
         <h1>Sign In</h1>
-        <input type="email" placeholder="Email..." />
-        <input type="password" placeholder="Password..." />
+        <input ref={emailRef} type="email" placeholder="Email..." />
+        <input ref={passwordRef} type="password" placeholder="Password..." />
         <button type="submit" onClick={signIn}>
           Sign In
         </button>
